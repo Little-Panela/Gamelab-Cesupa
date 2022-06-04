bc42d53f21b19967d89cfcdbe14a36a6cf970a59
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-06-04 14:39:40 -0300

    fixing responsive card

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -8,42 +8,54 @@ export function ProjectSection() {
 
       <ProjectsContainer>
         <div className="container">
-          <Card className="card">
-            <CardContent className="cardContent">
-              <iframe src="https://itch.io/embed/1540590" width="100%">
-                <a href="https://cesupagamelab.itch.io/blitroidz">
-                  BlitzRoids by Cesupa GameLab
-                </a>
-              </iframe>
-            </CardContent>
-          </Card>
-          <Card className="card">
-            <CardContent className="cardContent">
-              <iframe src="https://itch.io/embed/1540739" width="100%">
-                <a href="https://cesupagamelab.itch.io/barrelroll">
-                  Barrel Roll by Cesupa GameLab
-                </a>
-              </iframe>
-            </CardContent>
-          </Card>
-          <Card className="card">
-            <CardContent className="cardContent">
-              <iframe src="https://itch.io/embed/1525704" width="100%">
-                <a href="https://cesupagamelab.itch.io/office-boy">
-                  Office Boy by Cesupa GameLab
-                </a>
-              </iframe>
-            </CardContent>
-          </Card>
-          <Card className="card">
-            <CardContent className="cardContent">
-              <iframe src="https://itch.io/embed/1505014" width="100%">
-                <a href="https://cesupagamelab.itch.io/psicopato-a-origem">
-                  Psicopato: A origem by Cesupa GameLab
-                </a>
-              </iframe>
-            </CardContent>
-          </Card>
+          <div className="card">
+            <iframe
+              src="https://itch.io/embed/1540590"
+              width="100%"
+              height="100%"
+              frameBorder="0"
+            >
+              <a href="https://cesupagamelab.itch.io/blitroidz">
+                BlitzRoids by Cesupa GameLab
+              </a>
+            </iframe>
+          </div>
+          <div className="card">
+            <iframe
+              src="https://itch.io/embed/1540739"
+              width="100%"
+              height="100%"
+              frameBorder="0"
+            >
+              <a href="https://cesupagamelab.itch.io/barrelroll">
+                Barrel Roll by Cesupa GameLab
+              </a>
+            </iframe>
+          </div>
+          <div className="card">
+            <iframe
+              src="https://itch.io/embed/1525704"
+              width="100%"
+              height="100%"
+              frameBorder="0"
+            >
+              <a href="https://cesupagamelab.itch.io/office-boy">
+                Office Boy by Cesupa GameLab
+              </a>
+            </iframe>
+          </div>
+          <div className="card">
+            <iframe
+              src="https://itch.io/embed/1505014"
+              width="100%"
+              height="100%"
+              frameBorder="0"
+            >
+              <a href="https://cesupagamelab.itch.io/psicopato-a-origem">
+                Psicopato: A origem by Cesupa GameLab
+              </a>
+            </iframe>
+          </div>
         </div>
       </ProjectsContainer>
     </Container>
--- src/components/Projects/styles.ts
+++ src/components/Projects/styles.ts
@@ -13,40 +13,7 @@ export const Container = styled.div`
     font-size: 2.2rem;
   }
 `;
-
-export const SearchContainer = styled.div`
-  width: 90%;
-  height: 2rem;
-
-  display: flex;
-  justify-content: flex-end;
-  gap: 0.4rem;
-
-  margin-bottom: 0.6rem;
-  margin-top: 2rem;
-
-  button {
-    background: none;
-    border: none;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  input {
-    width: 18rem;
-    border-radius: 5px;
-    border: none;
-
-    padding: 1rem;
-
-    &::placeholder {
-      font-weight: 500;
-      opacity: 0.6;
-    }
-  }
-`;
-
+ 
 export const ProjectsContainer = styled.div`
   min-width: 90%;
   max-width: calc(100% - 5rem);
@@ -74,9 +41,10 @@ export const ProjectsContainer = styled.div`
     }
 
     .card {
-      min-width: 1rem;
-      padding: 0rem;
-
+      display: block;
+      overflow: hidden;
+      border-radius: 10px;   
+      
       @media (max-width: 800px) {
         & {
           width: 100%;
